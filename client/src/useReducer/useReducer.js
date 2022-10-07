6bfb4c24c0e9fed01c209cc3899cdeab18d41276
aniket848 <aniketmehrotra932@gmail.com>
2022-10-07 08:56:55 +0530

    some comment

--- client/src/useReducer/useReducer.js
+++
@@ -1,8 +0,0 @@
-export const reducer = (state,action)=>{
-    if(action.type === 'LOGIN')
-      return {isLoggedIn:true,userEmail:action.val};
-    else if(action.type === 'LOGOUT')  
-      return {isLoggedIn:false,userEmail:''};
-}
-
-export const initialState = {isLoggedIn:false,userEmail:''};
